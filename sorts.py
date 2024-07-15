f246977c0e54ef5f3f734c1430feabed458c4484
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-07-15 20:16:59 +0530

    blah

--- sorts.py
+++ sorts.py
@@ -1,37 +1,37 @@
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
+def quick_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    pivot = lst[len(lst) // 2]
+    left = [x for x in lst if x < pivot]
+    middle = [x for x in lst if x == pivot]
+    right = [x for x in lst if x > pivot]
     return quick_sort(left) + middle + quick_sort(right)
 
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
+def insertion_sort(lst):
+    for i in range(1, len(lst)):
+        key = lst[i]
         j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
+        while j >= 0 and key < lst[j]:
+            lst[j + 1] = lst[j]
             j -= 1
-        arr[j + 1] = key
-    return arr
+        lst[j + 1] = key
+    return lst
 
-def selection_sort(arr):
-    n = len(arr)
+def selection_sort(lst):
+    n = len(lst)
     for i in range(n):
         min_index = i
         for j in range(i + 1, n):
-            if arr[j] < arr[min_index]:
+            if lst[j] < lst[min_index]:
                 min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
+        lst[i], lst[min_index] = lst[min_index], lst[i]
+    return lst
 
-def bubble_sort(arr):
-    n = len(arr)
+def bubble_sort(lst):
+    n = len(lst)
     for i in range(n - 1):
         for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
-#harrye
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    return lst
+#hlstye
